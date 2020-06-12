d7f9fa0a8d043b24a7e91cc5a0e2f4b831742494
Akeme Ubom <programmer33@outlook.com>
2020-06-12 02:24:32 -0400

    First Commit
    Adding Original FIles

---
+++ web/index.js
@@ -0,0 +1,144 @@
+/*
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener
+var images = ["a.png", "b.png", "c.png"];
+var i = 0;
+$("#gallery").attr("src", images[i]);
+  setInterval(function(){
+    $("#gallery").attr("src", images[i]);
+      i++;
+    if (i > images.length){
+      i = 0;
+    }
+
+  }, 1000);
+*/
+const el = document.querySelector(".item");
+const img = document.querySelector("#gallery");
+
+var i = 1;
+
+  setInterval(function(){  
+    if (i > 8){
+      i = 1; 
+    }
+    $("#gallery").attr("src", `${i}.png`);
+    i++;
+
+  }, 2000);
+
+let isResizing = false;
+
+el.addEventListener("mousedown", mousedown);
+img.addEventListener("mousedown", mousedown);
+
+function mousedown(e) {
+  window.addEventListener("mousemove", mousemove);
+  window.addEventListener("mouseup", mouseup);
+
+  let prevX = e.clientX;
+  let prevY = e.clientY;
+
+  function mousemove(e) {
+    if (!isResizing) {
+      let newX = prevX - e.clientX;
+      let newY = prevY - e.clientY;
+
+      const rect = el.getBoundingClientRect();
+
+      el.style.left = rect.left - newX + "px";
+      el.style.top = rect.top - newY + "px";
+      img.style.left = rect.left - newX + "px";
+      img.style.top = rect.top - newY + "px";
+      prevX = e.clientX;
+      prevY = e.clientY;
+    }
+  }
+
+  function mouseup() {
+    window.removeEventListener("mousemove", mousemove);
+    window.removeEventListener("mouseup", mouseup);
+  }
+}
+
+
+const resizers = document.querySelectorAll(".resizer");
+let currentResizer;
+
+for (let resizer of resizers) {
+  resizer.addEventListener("mousedown", mousedown);
+
+  function mousedown(e) {
+    currentResizer = e.target;
+    isResizing = true;
+
+    let prevX = e.clientX;
+    let prevY = e.clientY;
+
+    window.addEventListener("mousemove", mousemove);
+    window.addEventListener("mouseup", mouseup);
+
+    function mousemove(e) {
+      const rect = el.getBoundingClientRect();
+
+      if (currentResizer.classList.contains("se")) {
+        el.style.width = rect.width - (prevX - e.clientX) + "px";
+        el.style.height = rect.height - (prevY - e.clientY) + "px";
+        img.style.width = rect.width - (prevX - e.clientX) + "px";
+        img.style.height = rect.height - (prevY - e.clientY) + "px";
+      } else if (currentResizer.classList.contains("sw")) {
+        el.style.width = rect.width + (prevX - e.clientX) + "px";
+        el.style.height = rect.height - (prevY - e.clientY) + "px";
+        el.style.left = rect.left - (prevX - e.clientX) + "px";
+        img.style.width = rect.width + (prevX - e.clientX) + "px";
+        img.style.height = rect.height - (prevY - e.clientY) + "px";
+        img.style.left = rect.left - (prevX - e.clientX) + "px";
+      } else if (currentResizer.classList.contains("ne")) {
+        el.style.width = rect.width - (prevX - e.clientX) + "px";
+        el.style.height = rect.height + (prevY - e.clientY) + "px";
+        el.style.top = rect.top - (prevY - e.clientY) + "px";
+
+        img.style.width = rect.width - (prevX - e.clientX) + "px";
+        img.style.height = rect.height + (prevY - e.clientY) + "px";
+        img.style.top = rect.top - (prevY - e.clientY) + "px";
+      } else {
+        el.style.width = rect.width + (prevX - e.clientX) + "px";
+        el.style.height = rect.height + (prevY - e.clientY) + "px";
+        el.style.top = rect.top - (prevY - e.clientY) + "px";
+        el.style.left = rect.left - (prevX - e.clientX) + "px";
+
+        img.style.width = rect.width + (prevX - e.clientX) + "px";
+        img.style.height = rect.height + (prevY - e.clientY) + "px";
+        img.style.top = rect.top - (prevY - e.clientY) + "px";
+        img.style.left = rect.left - (prevX - e.clientX) + "px";
+      }
+
+      prevX = e.clientX;
+      prevY = e.clientY;
+    }
+
+    function mouseup() {
+      window.removeEventListener("mousemove", mousemove);
+      window.removeEventListener("mouseup", mouseup);
+      isResizing = false;
+    }
+  }
+}
+/*
+@CarlosLunaGit */ 
